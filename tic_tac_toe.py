91bd281297483a484c216098c5790f6fed847188
Rhett Olson <olso9295@umn.edu>
2023-11-02 15:21:01 -0500

    Implementing actions and results functions for UltimateTicTackToe and TicTacToe classes

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,5 @@
 import copy
-
+import random
 class TicTacToeState:
     def __init__(self):
         self.grid = [["-" for i in range(3)] for j in range(3)]
@@ -12,7 +12,7 @@ class TicTacToeState:
                 string_board += self.grid[i][j] + " "
             string_board += "\n"
         if self.winner != None:
-            string_board += str(self.winner) + "has won.\n"
+            string_board += str(self.winner) + " has won.\n"
         else:
             string_board += self.to_move + " to move.\n"
         return string_board
@@ -24,6 +24,9 @@ class TicTacToe:
         for i in range(3):
             for char in ["X", "O"]:
                 if state.grid[i][0] == char and char == state.grid[i][1] and char == state.grid[i][2]:
+                    state.winner = char
+                    return True
+                if state.grid[0][i] == char and char == state.grid[1][i] and char == state.grid[2][i]:
                     state.winner = char
                     return True
         for char in ["X", "O"]:
@@ -41,15 +44,27 @@ class TicTacToe:
                 if state.grid[i][j] == "-":
                     actions_list.append([i, j])
         return actions_list
-    def result(state, player, action):
+    def result(state, action):
         state_copy = copy.deepcopy(state)
-        if state.grid[action[0]][action[1]] not in "XO":
+        if state.grid[action[0]][action[1]] in "XO":
             raise(Exception("Attempted to move onto already occupied square"))
-        state_copy.grid[action[0]][action[1]] = player
-        if player == "X":
+        state_copy.grid[action[0]][action[1]] = state.to_move
+        if state.to_move == "X":
             state_copy.to_move = "O"
-        if player == "O":
+        if state.to_move == "O":
             state_copy.to_move = "X"
+        if TicTacToe.is_terminal(state_copy):
+            state_copy.winner = state.to_move
+            state_copy.to_move = None
+        elif TicTacToe.actions(state_copy) == []:
+            state_copy.winner = "C"
+            state_copy.to_move = None
         return state_copy
 if __name__ == "__main__":
-    print(TicTacToeState())
+    board = TicTacToeState()
+    while board.winner == None:
+        print(board)
+        action = random.choice(TicTacToe.actions(board))
+        board = TicTacToe.result(board, action)
+    print(board)
+    
--- ultimate_tic_tac_toe.py
+++ ultimate_tic_tac_toe.py
@@ -1,10 +1,13 @@
 from tic_tac_toe import TicTacToeState, TicTacToe
+import copy
+import random
 
 class UltimateTicTacToeState:
     def __init__(self):
         self.subboard_grid = [[TicTacToeState() for i in range(3)] for j in range(3)]
         self.to_move = "X"
         self.winner = None
+        self.last_move = None
     def __repr__(self):
         outstring = ""
         for row in range(3):
@@ -22,21 +25,61 @@ class UltimateTicTacToeState:
         else:
             outstring += self.to_move + " to move."
         return outstring
-
-
+    
 class UltimateTicTacToe:
     def __init__(self):
         self.initial = UltimateTicTacToeState()
     def is_terminal(state):
         for i in range(3):
             for char in ["X", "O"]:
-                if state.grid[i][0] == char and char == state.grid[i][1] and char == state.grid[i][2]:
+                if state.subboard_grid[i][0].winner == char and char == state.subboard_grid[i][1].winner and char == state.subboard_grid[i][2].winner:
+                    return True
+                if state.subboard_grid[0][i].winner == char and char == state.subboard_grid[1][i].winner and char == state.subboard_grid[2][i].winner:
                     return True
         for char in ["X", "O"]:
-            if state.grid[0][0] == char and char == state.grid[1][1] and char == state.grid[2][2]:
+            if state.subboard_grid[0][0].winner == char and char == state.subboard_grid[1][1].winner and char == state.subboard_grid[2][2].winner:
                 return True 
-            if state.grid[2][0] == char and char == state.grid[1][1] and char == state.grid[0][2]:
+            if state.subboard_grid[2][0].winner == char and char == state.subboard_grid[1][1].winner and char == state.subboard_grid[0][2].winner:
                 return True 
         return False
+    def actions(state):
+        if state.last_move != None:
+            if state.subboard_grid[state.last_move[2]][state.last_move[3]].winner == None:
+                subboard_actions = TicTacToe.actions(state.subboard_grid[state.last_move[0]][state.last_move[1]])
+                return [(state.last_move[0], state.last_move[1], action[0], action[1]) for action in subboard_actions]
+            
+        return [(i, j, k, l) for i in range(3) for j in range(3) for k in range(3) for l in range(3) if (state.subboard_grid[i][j].winner == None and state.subboard_grid[i][j].grid[k][l] == "-")]
+    def result(state, action):
+        i, j, k, l = action
+        if state.subboard_grid[i][j].grid[k][l] != "-" or state.subboard_grid[i][j].winner != None:
+            raise(Exception("Attempted to move onto occupied space"))
+        state_copy = copy.deepcopy(state)
+        state_copy.subboard_grid[i][j].grid[k][l] = state.to_move
+        if state.to_move == "X":
+            state_copy.to_move = "O"
+        if state.to_move == "O":
+            state_copy.to_move = "X"
+        if TicTacToe.is_terminal(state_copy.subboard_grid[i][j]):
+            state_copy.winner = state.to_move
+            state_copy.to_move = None
+        elif TicTacToe.actions(state_copy.subboard_grid[i][j]) == []:
+            state_copy.winner = "C"
+            state_copy.to_move = None
+        if UltimateTicTacToe.is_terminal(state_copy):
+            state_copy.winner = state.to_move
+            state_copy.to_move = None
+        elif UltimateTicTacToe.actions(state_copy) == []:
+            state_copy.winner = "C"
+            state_copy.to_move = None
+        return state_copy
+            
+
 if __name__ == "__main__":
-    print(UltimateTicTacToeState())
+    board = UltimateTicTacToeState()
+    print(board)
+    while board.winner == None:
+        actions = UltimateTicTacToe.actions(board)
+        if (len(actions) > 9):
+            print("any subboard available to move")
+        board = UltimateTicTacToe.result(board, random.choice(actions))
+        print(board)
