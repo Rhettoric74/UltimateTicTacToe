4995f8724ddc94f86c05ec38c734e017d13f608b
Rhett Olson <olso9295@umn.edu>
2023-12-05 12:14:37 -0600

    Fixing backpropagation, recording results of experiments with random playout policies

--- monte_carlo.py
+++ monte_carlo.py
@@ -11,32 +11,61 @@ class MonteCarloNode:
         self.wins = 0
         self.successors = []
     def expand(node):
-        if node.successors == []:
+        if node.board.winner == None and node.successors == []:
             for action in node.actions:
-                node.successors.append(MonteCarloNode(UltimateTicTacToe.result(board, action)))
-    def simulate(node, target_winner = "X", playout_policy = random_agent):
-        simulated_game = UltimateTicTacToe.play_game(copy.deepcopy(node.board), playout_policy, playout_policy, True)
-        if simulated_game.winner == target_winner:
+                # add all successors to the successors list
+                node.successors.append(MonteCarloNode(UltimateTicTacToe.result(node.board, action)))
+    def random_policy(node):
+        return random.choice(node.successors)
+    def simulate(node, target_winner, playout_policy = random_policy):
+        simulation_path = [node]
+        cur_node = node
+        while cur_node.board.winner == None and cur_node.actions != []:
+            ''' print("Cur board")
+            print(cur_node.board) '''
+            MonteCarloNode.expand(cur_node)
+            cur_node = MonteCarloNode.random_policy(cur_node)
+            ''' print("updated board")
+            print(cur_node.board) '''
+            simulation_path.append(cur_node)
+        if cur_node.board.winner == target_winner:
+            return 1, simulation_path
+        if cur_node.board.winner == "C":
+            return 0.5, simulation_path
+        return 0, simulation_path
+    def win_rate_over_log_playouts(successor, simulations):
+        if successor.playouts == 0:
+            score = 1
+        else:
+            score = (successor.wins / successor.playouts) / math.log(successor.playouts + 2)
+        return score
+    def ucb_one(successor, simulations, c = math.sqrt(2)):
+        if successor.playouts == 0:
             return 1
-        return 0
-    def select(tree):
+        else:
+            return successor.wins / successor.playouts + c * math.sqrt(math.log(simulations) / successor.playouts)
+    def select(tree, simulations, selection_score = ucb_one):
         # TODO: try different policies for this
         # current policy: score nodes based on their win percentage divided by the natural log of the number of playouts plus 2
         # pick the node with the highest score
         best_score = 0
         best_node = None
         for successor in tree.successors:
-            if successor.playouts == 0:
-                score = 1
-            else:
-                score = (successor.wins / successor.playouts) / math.log(successor.playouts + 2)
+            score = selection_score(successor, simulations)
             if score >= best_score:
                 best_score = score
                 best_node = successor
         return best_node
-    def back_propagate(node, result):
-        node.wins += result
+    def back_propagate(node, result, target_winner):
+        added_wins, simulation_path = result
+        node.wins += added_wins
         node.playouts += 1
+        for visited_node in simulation_path:
+            if visited_node.board.to_move == target_winner:
+                visited_node.wins += added_wins
+                #print(visited_node.wins)
+            visited_node.playouts += 1
+
 def monte_carlo_player(board, time_limit = 1):
     print("Monte carlo tree search called")
     tree = MonteCarloNode(board)
@@ -46,10 +75,10 @@ def monte_carlo_player(board, time_limit = 1):
     simulations = 0
     while time.time() - start < time_limit:
         simulations += 1
-        leaf = MonteCarloNode.select(tree)
+        leaf = MonteCarloNode.select(tree, simulations)
         MonteCarloNode.expand(leaf)
         result = MonteCarloNode.simulate(leaf, target_winner)
-        MonteCarloNode.back_propagate(leaf, result)
+        MonteCarloNode.back_propagate(leaf, result, target_winner)
     print("simulations run:", simulations)
     best_action = None
     best_win_rate = - float("inf")
@@ -61,6 +90,7 @@ def monte_carlo_player(board, time_limit = 1):
         if (win_rate >= best_win_rate):
             best_win_rate = win_rate
             best_action = tree.actions[i]
+    print(best_win_rate)
     return best_action
 if __name__ == "__main__":
     wins_dict = {"X":0, "O":0, "C":0}
@@ -69,6 +99,7 @@ if __name__ == "__main__":
         result = UltimateTicTacToe.play_game(board, monte_carlo_player, random_agent)
         wins_dict[result.winner] += 1
     print(wins_dict)
-    with open("results.json", "w") as fw:
+    policy = "random_playout_seleciton_ucb_1"
+    with open("results/" + policy + ".json", "w") as fw:
         json.dump(wins_dict, fw)
         
